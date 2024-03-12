e2bf9e1e4dfd53b0c3a1aee8b71c8e09921934a3
Pedro Andrade <pedrogamerp@gmail.com>
2024-03-12 15:53:05 -0300

    feat: gateway entity

---
+++ IOTWebAPI/Entities/Gateway.cs
@@ -0,0 +1,14 @@
+namespace IOTWebAPI.Entities;
+
+public class Gateway
+{
+    public int GatewayID { get; set; }
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public bool IsActive { get; set; }
+    public string Type { get; set; }
+    public DateTime LastCommunication { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
